fa280e470202b244a7cd39f31f8a1d1e85c316c3
Salas <ga.e.m.project@gmail.com>
2014-12-30 23:08:27 +0800

    Go

---
+++ Summoner.py
@@ -0,0 +1,82 @@
+import re
+import json
+import pickle
+import requests
+from pymongo import *
+
+from util_method import *
+
+client = MongoClient()
+
+class Summoner:
+    def __init__(self, name):
+        self.games = client.lol.games
+        
+        self.summoner_name = name
+        self.id = self.getid()
+        self.data = self.load()
+        self.new_data_raw = []
+        self.tzinfo = TaipeiTimeZone()
+
+    def getid(self):
+        url = 'http://lol.moa.tw/summoner/show/' + self.summoner_name
+        response = requests.get(url)
+        id_obj = re.findall(r'MoaObj.lol.acctId = (.+);', response.text)
+        return id_obj[0] if id_obj else None
+
+    def get_recent_games(self):
+        try:
+            url = 'http://lol.moa.tw/Ajax/bs_recentgames/' + self.id + '/' + self.summoner_name
+            response = requests.get(url)
+            result = re.findall(r'var recentgames=(.+);', response.text)
+            self.json_data = json.loads(result[0])
+            self.get_recent_times()
+            self.data += [ e for e in self.new_data if e not in self.data ]
+            self.data = set( sorted(self.data) )
+            self.dump()
+        except Exception as e:
+            raise e
+        return self.data
+
+    def get_recent_times(self):
+        self.new_data = [ local_datetime(game['createDate'], self.tzinfo)
+                for game in self.json_data['gameStatistics'] ]
+
+    def load(self):
+        return [ r['date']
+                for r in self.games.find({ "summoner": self.summoner_name }) ]
+
+    def dump(self):
+        for time in sorted(self.data):
+            game_id = self.games.update(
+                    {
+                        "summoner": self.summoner_name,
+                        "date": time },
+                    {
+                        "summoner": self.summoner_name,
+                        "date": time
+                    },
+                    upsert=True)
+            print(game_id)
+
+    def clean_db(self):
+        self.games.remove({ "summoner": self.summoner_name })
+
+def main():
+    summoners = [
+                    '你根本在發廢文',
+                    'Salas',
+                    'Aragorn',
+                    '嵐辰星月夜',
+                    '性侵大使',
+                    '人品爆發揪咪030',
+                ]
+    for name in summoners:
+        print(name)
+        summoner = Summoner(name)
+        games = summoner.get_recent_games()
+        for result in summoner.games.find({ "summoner": name }):
+            print(result)
+
+if __name__ == '__main__':
+    main()
--- flask_test.py
+++ flask_test.py
@@ -1,10 +1,39 @@
 from flask import Flask
+from flask import render_template, request, redirect, url_for
+
+from Summoner import *
+
 app = Flask(__name__)
 
 @app.route('/')
-def hell_world():
-	return 'Hello World!'
+def index():
+    online_users = 1 # mongo.db.users.find({'online': True}).count()
+    print('online: ', online_users)
+    return render_template('index.html',
+            online_users=online_users)
+
+@app.route('/summoner/<username>')
+def show_user_profile(username):
+    return query(username)
+
+@app.route('/summoner/', methods=['GET', 'POST'])
+def action_form():
+    if request.method == 'POST':
+        username = request.form['query_name']
+        return redirect(url_for('show_user_profile', username=username))
+    else:
+        return render_template('form.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+        return render_template('page_not_found.html'), 404
+
+def query(name):
+    s = Summoner(name)
+    games = s.get_recent_games()
+    return render_template('user.html',
+            username=name, games=games)
 
 if __name__ == '__main__':
-	app.run()
+    app.run(debug=True)
 
