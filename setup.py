b140df95ba8c4a6b26f965c53cadea6f3564b1e4
Salas <ga.e.m.project@gmail.com>
2014-12-30 23:20:37 +0800

    ??

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ setup(name='LOL-UCCU',
       author='Salas',
       author_email='salas@salas.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask>=0.10.1'
-			'pymongo>=2.7'
+      install_requires=['Flask>=0.10.1',
+			'Flask-PyMongo>=0.3.0',
+			'pymongo>=2.7.2',
+			'requests>=2.5.0'
 			],
      )
