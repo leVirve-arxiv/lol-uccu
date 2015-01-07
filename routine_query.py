f2582f206daebf92484ed4a50baa10a051a8ff6f
Salas <ga.e.m.project@gmail.com>
2015-01-08 00:22:22 +0800

    routine user added

--- routine_query.py
+++ routine_query.py
@@ -2,7 +2,16 @@
 from Summoner import *
 
 def main():
-    names = ["你根本在發廢文"]
+    names = [
+                "你根本在發廢文",
+                "嵐辰星月夜",
+                "性侵大使",
+                "人品爆發揪咪030",
+                "戰神露特娜",
+                "Aragorn",
+                "Salas",
+                "HappyStrom",
+            ]
     for name in names:
         result = Summoner(name).get_recent_games()
         print(result)
